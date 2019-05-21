bcbb8a57464d2f53676399c16586f17e469c995d
sunshane6726 <sunshane6726@gmail.com>
2019-05-21 21:23:16 +0900

    Admin

--- projectSecond_root/blog/models.py
+++ projectSecond_root/blog/models.py
@@ -7,6 +7,9 @@ class Blog(models.Model):
     pub_date = models.DateTimeField('date published') # 시간과 관련된 함수를 쓰고 
     body = models.TextField() # 엄청 긴글을 쓸수 있는 박스
 
+    def __str__(self): # 메소드를 확인 해준다. __ 특수메소드 사용
+        return self.title
+
 
 # python manage.py makemigration 파이썬 >> 쿼리 해석
 # python manage.py migration >> 해석한것을 전달 
